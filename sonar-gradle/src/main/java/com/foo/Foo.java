87120215dfab0c80089c6430e79984a92d9664fe
Fred Duarte <fred.duarte@qualifacts.com>
2016-10-25 14:58:24 -0500

    package

---
+++ sonar-gradle/src/main/java/com/foo/Foo.java
@@ -0,0 +1,14 @@
+package com.foo;
+
+public class Foo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public String someLibraryMethod(){
+		return "test";
+	}
+
+}
