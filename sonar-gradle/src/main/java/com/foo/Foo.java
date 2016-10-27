39155011c0267db54d1e0c937d61166a0b73a93e
Fred Duarte <fred.duarte@qualifacts.com>
2016-10-27 14:58:43 -0500

    Update

--- sonar-gradle/src/main/java/com/foo/Foo.java
+++ sonar-gradle/src/main/java/com/foo/Foo.java
@@ -8,7 +8,8 @@ public class Foo {
 	}
 
 	public void coveredByUnitTest() {
-		System.out.println("Hello, world.");
+		System.out.println("Hello, world.1");
+		System.out.println("Hello, world.2");
 	}
 
 	public void notCoveredByUnitTest() {
